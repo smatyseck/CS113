8d7f925b98d84273a1a7f537d8320f8f7f7f3a0a
Samuel Matyseck <smatyseck@gmail.com>
2018-10-25 12:35:22 -0700

    Implemented 2 Player Controls, 2 Player swapping , Swap Grace Period
    Player 1 uses a,d, space for movement and left ctrl for swap
    Player 2 uses arrow keys for movement and right ctrl for swap
    Swapping now requires both players to press their swap button within 3 seconds of each other (there is a timer on the screen)

---
+++ Assets/Scripts/SwapOff.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwapOff : MonoBehaviour {
+
+    public float interval = 3f;
+    private float timeremaining;
+    public GameObject player1;
+    public GameObject player2;
+
+    // Use this for initialization
+    void Start () {
+        timeremaining = interval;
+        gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.activeSelf && timeremaining <= 0)
+        {
+            player1.GetComponent<Player1Movement>().SetReady(false);
+            player2.GetComponent<Player2Movement>().SetReady(false);
+            gameObject.SetActive(false);
+            timeremaining = interval;
+        }
+
+        if (gameObject.activeSelf)
+        {
+            Text text = gameObject.GetComponent<Text>();
+            timeremaining -= Time.deltaTime;
+            if (player1.GetComponent<Player1Movement>().IsReady())
+            {
+                text.text = "Player 1 is Ready to Swap\n" + Mathf.Ceil(timeremaining).ToString();
+            } else if (player2.GetComponent<Player2Movement>().IsReady())
+            {
+                text.text = "Player 2 is Ready to Swap\n" + Mathf.Ceil(timeremaining).ToString();
+            }
+        }
+        
+	}
+
+}
